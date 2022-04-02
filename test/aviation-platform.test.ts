470ca4f620034a0f7ace40649ed089023dfb795f
methugop <hugo.page@metservice.com>
2022-04-02 20:20:39 +1300

    Initial commit

---
+++ test/aviation-platform.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AviationPlatform from '../lib/aviation-platform-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aviation-platform-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AviationPlatform.AviationPlatformStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
